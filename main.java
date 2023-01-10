dd764459ed1037ab9291715dc4f01264824107f2
KeddyB <bokelusi@gmail.com>
2023-01-10 15:44:18 +0100

    day 30 or 31 i think. first java

---
+++ main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int age = 12;
+        System.out.println("hi, my name is busayo and i am "+ age +" years old");
+    }
+}
