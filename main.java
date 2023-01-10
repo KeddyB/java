58ebe18c1ada709821702fddeed56424724aa6e5
KeddyB <bokelusi@gmail.com>
2023-01-10 15:55:57 +0100

    java time and all starting soon

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 class Main {
+    static int age = 21;
     public static void main(String[] args){
-        System.out.println("Hello world");
-        int age = 12;
         System.out.println("hi, my name is busayo and i am "+ age +" years old");
     }
 }
