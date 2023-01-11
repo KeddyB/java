e36214c97745336bf7139549cbf28c2beb729183
KeddyB <bokelusi@gmail.com>
2023-01-11 10:47:03 +0100

    learning java, this is the first branch

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main {
     static int age = 21;
     public static void main(String[] args){
-        System.out.println("hi, my name is busayo and i am "+ age +" years old");
+        //this is supposed to be for weekends
     }
 }
