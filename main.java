3cc1e1beb1b77feabc945254c036005123e3a8f3
KeddyB <bokelusi@gmail.com>
2023-01-22 14:02:00 +0100

    learning java at the moment

--- main.java
+++ main.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 class Main {
     static int age;
     public static void main(String[] args){
-        
-        int score = 0;
-        int turns = 10;
-        for(int i = 0; i<=turns; i++){
-            if (turns == i) {
-                System.out.println("ÿour score " + score++);
-            } else {
-                System.out.println("your turn "+ turns--);
-            }
-        };
+        String string = "the sky is blue";
+        System.out.println(string.replace("blue", "red"));
+
+        String updatedString = string.replace("blue", "red");
+        System.out.println(updatedString);
+
+        System.out.println(string.contains(" "));
+
+        //taking input from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("what is your name?");
+        String name = scanner.nextLine();
+        System.out.println("your name is " + name + ". How old are you?");
+        int age = scanner.nextInt();
+        System.out.printf("You are %d years old", age);
+        scanner.close();
+
+        //print doesnt have a new line character so the next line prints on the same line
+        //printf help to print formatted strings %s for string %d for integer 
     }
  }
