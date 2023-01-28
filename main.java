e1ae788d9643d6001265907d812a4cf8fb0bb570
KeddyB <bokelusi@gmail.com>
2023-01-29 00:38:19 +0100

    day 29 second commit.. good night

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Main {
     static int age;
     public static void main(String[] args){
-        String string = "the sky is blue";
+        /*String string = "the sky is blue";
         System.out.println(string);
 
         String updatedString = string.replace("blue", "red");
@@ -17,7 +17,7 @@ class Main {
         System.out.println("your name is " + name + ". How old are you?");
         int age = scanner.nextInt();
         System.out.printf("You are %d years old", age);
-        scanner.close();
+        scanner.close(); */
 
         //print doesnt have a new line character so the next line prints on the same line
         //printf help to print formatted strings %s for string %d for integer 
@@ -28,10 +28,24 @@ class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the first number");
         double num1 = scan.nextDouble();
+        scan.nextLine();
 
         System.out.println("Enter the second number");
         double num2 = scan.nextDouble();
+        scan.nextLine();
 
         System.out.println("what operation do you want to perform");
+        String operation = scan.nextLine();
+
+        if(operation.equals("sum")){
+            System.out.printf("%f + %f = %f", num1, num2, num1 + num2);
+        }else if(operation.equals("subtract")){
+            System.out.printf("%f - %f = %f", num1, num2, num1-num2);
+        }else if(operation.equals("multiply")){
+            System.out.printf("%f * %f = %f", num1, num2, num1 * num2);
+        }else if(operation.equals("divide")){
+            System.out.printf("%f / %f = %f", num1, num2, num1 / num2);
+        };
+        scan.close();
     }
  }
